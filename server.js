fbfa6a907c16183507552c51137ab58df83a401d
alivasilchikova <Alina-vasilchikova@live.com>
2016-12-08 16:06:01 +0300

    3

---
+++ server.js
@@ -0,0 +1,10 @@
+// Create an app
+var server = require('diet')
+var app = server()
+app.listen('http://localhost:8000')
+
+// When http://localhost:8000/ is requested, respond with "Hello World!"
+app.get('/', function($){
+    $.end('Hello World!')
+})
+
